e4a0fa91af1c5c58c9e17d0eb3a60ac5421569b4
havocblast <me@jacobkruse.com>
2025-05-29 15:23:54 -0400

    Adds Wezterm configs

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font_size = 18.0
+config.color_scheme = "Catppuccin Macchiato"
+--config.window_background_opacity = 0.6
+return config
